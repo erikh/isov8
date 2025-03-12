f6e2e8e71049b80b4da863694e7a51d1f1fd253f
Erik Hollensbe <git@hollensbe.org>
2025-03-11 19:39:18 -0700

    more challenges
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -113,5 +113,16 @@ mod tests {
         map.insert(Value::String("test".to_string()), Value::Float(1.0));
         let o = Value::Object(map);
         assert_eq!(result, o);
+        // let result = iso
+        //     .eval(
+        //         r#"
+        //     function foo() {
+        //         1
+        //     }
+        //     foo()
+        //     "#,
+        //     )
+        //     .unwrap();
+        // assert_eq!(result, Value::Float(1.0));
     }
 }
